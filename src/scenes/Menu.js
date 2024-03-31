cbd50588dd7496fe8dc0e19f1e5e29121a0a986d
Igor Bessa <ibessa@ucsc.edu>
2024-03-30 18:49:21 -0700

    added more deliverables

---
+++ src/scenes/Menu.js
@@ -0,0 +1,17 @@
+class Menu extends Phaser.Scene{
+    constructor(){
+        super('menuScene');
+    }
+
+    preload(){
+
+    }
+
+    create(){
+        this.cameras.main.setBackgroundColor(0,0,0);
+    }
+
+    update(){
+        
+    }
+}
---
+++ src/scenes/Night1.js
@@ -0,0 +1,13 @@
+class Night1 extends Phaser.Scene{
+    constructor(){
+        super('night1Scene');
+    }
+
+    preload(){
+
+    }
+
+    update(){
+        
+    }
+}
