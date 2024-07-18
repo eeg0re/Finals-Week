3fe3da643ba7ea573168b5a490e0e2f33debb830
Igor Bessa <ibessa@ucsc.edu>
2024-07-17 22:31:24 -0700

    Improved loading screen and placeholder menu screen

--- src/scenes/Load.js
+++ src/scenes/Load.js
@@ -6,16 +6,16 @@ class Load extends Phaser.Scene{
     preload(){
         // set up loading bar
         // see: https://rexrainbow.github.io/phaser3-rex-notes/docs/site/loader/
-        let loading = this.add.graphics();
-        // see: https://photonstorm.github.io/phaser3-docs/Phaser.Loader.Events.html
-        this.load.on('progress', (value)=> {
-            loading.clear();                            // reset fill/line style
-            loading.fillStyle(0x911C1C, 1);             // (color, alpha)
-            loading.fillRect(30, 300, game.config.width*value, 15);  // (x, y, width, height)
-        });
-        this.load.on('complete', ()=> {
-            loading.destroy();
-        });
+        // let loading = this.add.graphics();
+        // // see: https://photonstorm.github.io/phaser3-docs/Phaser.Loader.Events.html
+        // this.load.on('progress', (value)=> {
+        //     loading.clear();                            // reset fill/line style
+        //     loading.fillStyle(0x911C1C, 1);             // (color, alpha)
+        //     loading.fillRect(30, 300, game.config.width*value, 15);  // (x, y, width, height)
+        // });
+        // this.load.on('complete', ()=> {
+        //     loading.destroy();
+        // });
 
         // set the load path
         this.load.path = './assets/'
@@ -23,11 +23,50 @@ class Load extends Phaser.Scene{
         // load images
         this.load.image('empty room', 'isometric room 1.png');
 
+        this.load.image("title_bg", "title_bg.jpg");
+
+        this.load.image("options_button", "options_button.png");
+
+        this.load.image("play_button", "play_button.png");
+
+        this.load.image("logo", "logo.png");
+
+        this.load.spritesheet("cat", "cat.png", {
+            frameHeight: 32, 
+            frameWidth: 32
+        });
+
+        // create the loading bar
+        let loading = this.add.graphics({
+            fillStyle: {
+                color: 0xffffff // white    
+            }
+        });
+        // loader events: 
+        //     complete - when done loading everything
+        //     progress - loader number progress is in decimal 
+        this.load.on("progress", (percent)=> {
+            loading.fillRect(0, this.game.renderer.height/2, this.game.renderer.width * percent, 50);
+            console.log(percent);
+        });
+
+        this.load.on("complete", ()=>{
+            this.scene.start('menuScene');
+        });
+
+        // // simulate large load
+        // for(let i = 0; i < 200; i++){
+        //     this.load.spritesheet("cat" + i, "cat.png", {
+        //         frameHeight: 32, 
+        //         frameWidth: 32
+        //     });
+        // }
+
 
     }
 
     create(){
-        this.scene.start('menuScene');
+
     }
 
     update(){
--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -11,11 +11,44 @@ class Menu extends Phaser.Scene{
         // set up keyboard input
         cursors = this.input.keyboard.createCursorKeys();
 
-        // add background
-        this.cameras.main.setBackgroundColor(0,0,0);
+        //f ================ for when i have music ============================
+        //  this.sound.pauseOnBlur = false;
+        //  this.sound.play("title_music", {
+        //     loop: true
+        // })
+        // ====================================================================
 
-        // add text 
-        
+        // add background
+        //this.cameras.main.setBackgroundColor(0,0,0);
+        this.add.image(0,0, "title_bg").setOrigin(0).setDepth(0);
+        // add logo
+        this.add.image(this.game.renderer.width/2, this.game.renderer.height*0.2, "logo").setDepth(1);
+        let playButton = this.add.image(this.game.renderer.width/2, this.game.renderer.height / 2, "play_button").setDepth(1); 
+        this.add.image(this.game.renderer.width/2, this.game.renderer.height / 2 + 100, "options_button").setDepth(1); 
+
+        // create sprites
+        let hoverSprite = this.add.sprite(100, 100, "cat");
+        hoverSprite.setScale(2);
+
+
+        // make play button interactive
+        playButton.setInteractive();
+
+        playButton.on("pointerover", ()=>{
+            hoverSprite.setVisible(true);
+            hoverSprite.x = playButton.x - playButton.width;
+            hoverSprite.y = playButton.y
+        });
+
+        playButton.on("pointerout", ()=>{
+            hoverSprite.setVisible(false);
+            console.log("outta here");
+        });
+
+        playButton.on("pointerup", ()=>{
+            console.log("open na noor");
+        });
+
 
     }
 
